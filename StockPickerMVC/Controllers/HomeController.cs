5e5a9a7fb37059db17f4b8f3bdaa857777ce79ed
Caleb Smith <caleb.charles.smith23@gmail.com>
2022-10-21 21:21:05 -0500

    Deleted Unused files added a csv stock data file
    Deleted files that were not being used added and corrected a csv file with s&p 500 stock data created an ASP.Net MVC with a stock class and filegateway.

---
+++ StockPickerMVC/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StockPickerMVC.Models;
+
+namespace StockPickerMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            FileGateway gateway = new FileGateway();
+            List<Stock> stocks = gateway.GetStocks("C:\\Users\\caleb\\OneDrive\\Documents\\Computer Science Classes\\GitHub\\StockPicker\\constituents-financials_csv.csv");
+
+            ViewBag.Stocks = stocks;
+
+            return View();
+        }
+    }
+}
---
+++ StockPickerMVC/Models/Stock.cs
@@ -0,0 +1,53 @@
+﻿// Caleb Smith
+// 10/21/2022
+namespace StockPickerMVC.Models
+{
+    public class Stock
+    {
+        // Class variables
+        private string companyName = "na";
+        private string ticker = "na";
+        private string marketCap = "na";
+
+        // gets and sets
+        public string CompanyName
+        {
+            get { return companyName; }
+            set { companyName = value; }
+        }
+        public string Ticker
+        {
+            get { return ticker; }
+            set { ticker = value; }
+        }
+        public string MarketCap
+        {
+            get { return marketCap; }
+            set { marketCap = value; }
+        }
+
+        // Constructors
+        public Stock()
+        {
+
+        }
+        public Stock(string CompanyName, string Ticker)
+        {
+            this.CompanyName = CompanyName;
+            this.Ticker = Ticker;
+        }
+        public Stock(string CompanyName, string Ticker, string MarketCap)
+        {
+            this.CompanyName = CompanyName;
+            this.Ticker = Ticker;
+            this.MarketCap = MarketCap;
+        }
+
+        // methods
+        public override string ToString()
+        {
+            string message = $"Company Name: {CompanyName}  Ticker: {Ticker}  Market Cap: {MarketCap}";
+            return message;
+        }   
+    }
+}
